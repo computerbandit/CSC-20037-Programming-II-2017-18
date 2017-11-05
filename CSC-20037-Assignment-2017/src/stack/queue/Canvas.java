986778a07ecd42d64b58c7fec0c5589ad2bf3304
JemsCo <james.copping97@gmail.com>
2017-11-05 13:32:24 +0000

    new changes

--- CSC-20037-Assignment-2017/src/stack/queue/Canvas.java
+++ CSC-20037-Assignment-2017/src/stack/queue/Canvas.java
@@ -13,7 +13,7 @@ import javax.swing.JPanel;
  * @author w4f21
  */
 public class Canvas extends JPanel {
-
+    //this is just a quick test
     StackQueue app;
 
     public Canvas(StackQueue app) {
